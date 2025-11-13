c1e60dbc059d805f0d70cdaa63d27814426b96f5
Ronni-Grand <ronnietakespics@gmail.com>
2025-11-12 20:18:49 -0600

    Added a contact form using Form Spree, added a footer and started to plan to personalize.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/layouts/Navbar.jsx";
+import Footer from "./components/layouts/Footer.jsx";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Outlet />
       </main>
       {/* Footer goes below */}
+      <Footer />
     </div>
   );
 }
---
+++ src/components/layouts/Footer.jsx
@@ -0,0 +1,15 @@
+import Section from "../ui/Section.jsx";
+import Container from "../ui/Container.jsx";
+
+export default function Footer() {
+  return (
+    <Section className="border-t border-white/10 text-center">
+      <Container>
+        <p className="mt-2 text-sm">
+          &copy; {new Date().getFullYear()} Ronni Grandberry. All Rights
+          Reserved.
+        </p>
+      </Container>
+    </Section>
+  );
+}
